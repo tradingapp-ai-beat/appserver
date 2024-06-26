7ae73529047f0563f18e92232cb3b7ae6c7564d8
tradingapp-ai-beat <gamllercontroller@gmail.com>
2024-06-26 16:18:40 +0100

    server12

--- server.js
+++ server.js
@@ -25,27 +25,6 @@ jobs:
     - name: Login to Azure
       run: az login --service-principal -u ${{ secrets.AZURE_CLIENT_ID }} -p ${{ secrets.AZURE_CLIENT_SECRET }} --tenant ${{ secrets.AZURE_TENANT_ID }}
 
-    - name: Setup Azure PowerShell
-      uses: azure/powershell@v2
-      with:
-        azPSVersion: 'latest'
-
-    - name: Run Azure PowerShell script
-      run: |
-        $ErrorActionPreference = 'Stop'
-        $WarningPreference = 'SilentlyContinue'
-
-        # Get the access token from Azure CLI
-        $azAccessToken = az account get-access-token --resource https://management.azure.com/ | ConvertFrom-Json
-        $token = $azAccessToken.accessToken
-
-        # Use the token to authenticate in PowerShell
-        Connect-AzAccount -AccessToken $token -AccountId ${{ secrets.AZURE_CLIENT_ID }} -TenantId ${{ secrets.AZURE_TENANT_ID }}
-
-        # Your PowerShell script here
-        Get-AzResourceGroup
-      shell: pwsh
-
     - name: Deploy to Azure Web App
       uses: azure/webapps-deploy@v2
       with:
